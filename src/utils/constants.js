fbc4b2c62321832d23fab685663dbac30058e07a
Suveena-katy <suveena.victor@method-hub.com>
2026-06-05 18:24:37 +0530

    stakeholder

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -50,9 +50,9 @@ export const DASHBOARD_UUID_MAP = {
   account_manager: '2601d9f3-cf7d-4a79-a8f5-1d090874dbae',
   accountmanager: '2601d9f3-cf7d-4a79-a8f5-1d090874dbae',
   'account manager': '2601d9f3-cf7d-4a79-a8f5-1d090874dbae',
-  business_stakeholder: '2601d9f3-cf7d-4a79-a8f5-1d090874dbae',
-  businessstakeholder: '2601d9f3-cf7d-4a79-a8f5-1d090874dbae',
-  'business stakeholder': '2601d9f3-cf7d-4a79-a8f5-1d090874dbae',
-  stakeholder: '2601d9f3-cf7d-4a79-a8f5-1d090874dbae',
+  business_stakeholder: 'ed6516d5-8558-40c4-a022-8752bc0beb53',
+  businessstakeholder: 'ed6516d5-8558-40c4-a022-8752bc0beb53',
+  'business stakeholder': 'ed6516d5-8558-40c4-a022-8752bc0beb53',
+  stakeholder: 'ed6516d5-8558-40c4-a022-8752bc0beb53',
   default: '62ae3ed4-ca6c-49bf-99ed-d02afa453d9f',
 };
